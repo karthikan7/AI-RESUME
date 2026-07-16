25e33705544246df890c9d06ce655ca55e706fdb
Karthik A N <ankarthik657@gmail.com>
2026-07-16 23:50:06 +0530

    Update API URL for production

--- Frontend/src/features/interview/services/interview.api.js
+++ Frontend/src/features/interview/services/interview.api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "http://localhost:3000",
+    baseURL: "https://ai-resume-1-v6jj.onrender.com",
     withCredentials: true,
 })
 
